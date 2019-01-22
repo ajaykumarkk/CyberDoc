b409a532c8556b4ca2d4f58c7041ba3ea8f4d4fd
Ajay Kumar K K <ajaykumarkk77@gmail.com>
2019-01-22 11:43:02 +0530

    Add files via upload

---
+++ processextract.py
@@ -0,0 +1,95 @@
+import psutil
+import hashlib
+import sqlite3
+from sqlite3 import Error
+from win32com.client import Dispatch
+
+def md5Checksum(filePath):
+    with open(filePath, 'rb') as fh:
+        m = hashlib.md5()
+        while True:
+            data = fh.read(8192)
+            if not data:
+                break
+            m.update(data)
+        return m.hexdigest()
+
+def dbConnect():
+	conn = sqlite3.connect('History_Data.db')
+	return conn
+	print("Connected Successfully")
+
+def dbCreate():
+	conn=dbConnect()
+	conn.execute(''' CREATE TABLE if not exists Bad_Hash (id INTEGER PRIMARY KEY AUTOINCREMENT ,md5 STRING);''')
+	conn.execute(''' CREATE TABLE if not exists VT (id INTEGER PRIMARY KEY AUTOINCREMENT ,md5 STRING,total INTEGER,pos INTEGER );''')
+	conn.execute(''' CREATE TABLE if not exists phashes_q (id INTEGER PRIMARY KEY AUTOINCREMENT ,md5 STRING, UNIQUE(md5));''')
+
+	#conn.execute(''' CREATE TABLE if not exists Drives (id INTEGER PRIMARY KEY AUTOINCREMENT ,DriveName STRING,FileSystem STRING,Serial STRING,Size STRING);''')
+
+	#print("Table Created Successfully")
+
+def dbInsert(d):
+	conn=dbConnect()
+	c=conn.cursor()
+	c.execute('INSERT INTO Bad_Hash(id,md5) values(NULL,"'+d+'");')
+	conn.commit()
+	conn.close()
+	print("Inserted TO DB : "+d)
+
+def dbInsert(d,t,p):
+	conn=dbConnect()
+	c=conn.cursor()
+	c.execute('INSERT INTO VT(id,md5,total,pos) values(NULL,"'+d+'",'+t+','+p+');')
+	conn.commit()
+	conn.close()
+	print("Inserted TO DB : "+d)
+	
+def dbInsertp_q(d):
+	conn=dbConnect()
+	c=conn.cursor()
+	c.execute('INSERT OR IGNORE INTO phashes_q(id,md5) values(NULL,"'+d+'");')
+	conn.commit()
+	conn.close()
+	print("Inserted TO DB : "+d)
+	
+def dbsearch(d):
+	conn=dbConnect()
+	c=conn.cursor()
+	cout=c.execute('select * from Bad_Hash where md5 = "'+d+'";')
+	rows = cout.fetchall()
+	if len(rows) == 0:
+		return False
+	else:
+		return True
+
+def getversioninfo(path):
+	ver_parser = Dispatch('Scripting.FileSystemObject')
+	info = ver_parser.GetFileVersion(path)
+	#print(vars(ver_parser))
+	if info == 'No Version Information Available':
+		info = None
+	return info
+
+
+if __name__ == '__main__':
+	dbCreate()
+	proc_dict={p.pid: p for p in psutil.process_iter()}
+	print("Total no.of prpocess"+str(len(proc_dict.keys())))
+	for id in proc_dict.keys():
+		hash_temp=""
+		try:
+			#print("Checking for process"+proc_dict[id].name())
+			hash_temp=md5Checksum(proc_dict[id].exe())
+			dbInsertp_q(hash_temp)
+			if dbsearch(hash_temp) == True:
+				print("Killing process"+proc_dict[id].name())
+				proc_dict[id].kill()
+			else:
+				#print("-->"+proc_dict[id].name()+" : "+getversioninfo(proc_dict[id].exe()))
+				getversioninfo(proc_dict[id].exe())
+				
+		except Exception as e:
+			#print(e)
+			#print(proc_dict[id].name())
+			pass
---
+++ test.py
@@ -0,0 +1,129 @@
+import requests
+import time
+import csv
+import sys
+import sqlite3
+
+class GetOutOfLoop( Exception ):
+    pass
+
+def dbConnect():
+	conn = sqlite3.connect('History_Data.db')
+	return conn
+	print("Connected Successfully")
+	
+def dbInsert(d,t,p):
+	conn=dbConnect()
+	c=conn.cursor()
+	c.execute('INSERT INTO VT(id,md5,total,pos) values(NULL,"'+d+'",{},{});'.format(t,p))
+	conn.commit()
+	conn.close()
+	print("Inserted TO DB : "+d)
+	
+def dbsearch(d):
+	conn=dbConnect()
+	c=conn.cursor()
+	cout=c.execute('select * from VT where md5 = "'+d+'";')
+	rows = cout.fetchall()
+	if len(rows) == 0:
+		return False
+	else:
+		return True
+		
+def getpdata():
+	conn=dbConnect()
+	c=conn.cursor()
+	cout=c.execute('select md5 from phashes_q;')
+	rows = cout.fetchall()
+	if len(rows) == 0:
+		return False
+	else:
+		return rows
+		
+def getdata(hash,apikey):
+	params = {'apikey': apikey, 'resource':hash}
+	headers = {"Accept-Encoding": "gzip, deflate","User-Agent" : "gzip,  My Python requests library example client or username"}
+	response_dict={}
+	try:
+		r = requests.get('https://www.virustotal.com/vtapi/v2/file/report', params=params)
+		if r.status_code == 403:
+			return "Forbidden. You don't have enough privileges to make the request"
+		elif  r.status_code == 204:
+			return "Request rate limit exceeded"
+		elif r.status_code == 400:
+			return "Bad Request"
+		elif r.status_code == 200:
+			response_dict = r.json()
+			return response_dict
+	except Exception as e:
+		return "API Request Error"
+	return response_dict
+
+
+def checkvt(lines):
+	apikeys=['08074dd7e431fa9f6bc342947e4707099c4adcfb4b72090286ed24fc9437f95f','924d105b1634c233f2f72d890fd1340b98cefafe6ef6939f2c88e9cf4eecdf47','c476f9625b273f5e4b4f3c3c4e8adbc33899cf2bcdc695b0ba8cb30cdd01b7f1']
+	#print("Got the keys and list"+str(lines))
+	if len(apikeys) <= 6 :
+		waitime = (60 - len(apikeys) * 4)
+	else:
+		waitime = 0
+	el_flag=True
+	hashes = iter(lines)
+	#print(lines)
+	try:
+		while el_flag:
+			for api_key in apikeys:
+				for i in range(0,4):
+					response_dict={}
+					hash=""
+					try:#getting hashes from iterator
+						while True:
+							hash = next(hashes)
+							if dbsearch(hash) == False:
+								print("Breaking..")
+								break
+					except:
+						print("End of list")
+						el_flag=False
+						raise GetOutOfLoop
+						
+					response_dict=getdata(hash,api_key)
+					sample_info={}
+					if isinstance(response_dict, str):
+						#print("request error for hash :"+hash)
+						print("-->"+response_dict+" for Hash "+hash)
+						if response_dict == "Request rate limit exceeded":
+							print("Changing api key..")
+							unprocessed.append(hash)
+							break
+					elif isinstance(response_dict,dict) and response_dict.get("response_code") == 0:
+						#print("Not in VT for hash :"+str(hash))
+						notinvt.append(hash)
+					elif isinstance(response_dict,dict) and response_dict.get("response_code") == -2:
+						print("In queue for scanning")
+					elif isinstance(response_dict,dict) and response_dict.get("response_code") == 1:
+						# Hashes
+						sample_info["md5"] = response_dict.get("md5")
+						# AV matches
+						sample_info["positives"] = response_dict.get("positives")
+						sample_info["total"] = response_dict.get("total")
+						#csv_handle.write(sample_info["md5"]+","+str(sample_info["positives"])+","+str(sample_info["total"]))
+						print(sample_info["md5"]+","+str(sample_info["positives"])+","+str(sample_info["total"]))
+						dbInsert(sample_info["md5"],sample_info["total"],sample_info["positives"])
+						#csv_handle.write('\n')
+					else:
+						print("Unknown Error for hash "+hash)
+						unprocessed.append(hash)
+				print("API KEY : "+str(api_key)+" has ran 4 times.. Changing APi Key..")
+			print("WaitTime is "+str(waitime)+" Seconds")
+			for i in range(1,waitime):
+				print(i,end="\r")
+				time.sleep(1)
+	except:
+		pass
+pdat = 	getpdata()		
+if pdat == False:
+	pass
+else:
+	l=[h[0] for h in pdat] 
+	checkvt(l)
